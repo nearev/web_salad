6b91df629f586feafbb1ec747f17f806ced909e0
Axell MS <axellms18@gmail.com>
2022-07-27 23:44:24 +0700

    Created Footer and FooterContact + CSS Tracking Widest

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 import "./App.css";
 import Footer from "./components/bottombar/Footer";
-
+import FooterContact from "./components/bottombar/FooterContact";
 import Navbar from "./components/topbar/Navbar";
 
 function App() {
     return (
-        <div className="App">
+        <div className="App tracking-widest">
             <Navbar />
             <div>
                 <div className="flex-grow mb-auto">Hello world</div>
                 <div>sdahdhassjda</div>
                 <div>sdahdhassjda</div>
             </div>
-
+            <FooterContact />
             <Footer />
         </div>
     );
