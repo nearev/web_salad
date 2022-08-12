663ac7558804ce21f14628459be17a0eb7352921
Jason Limas <jasonandrea14@gmail.com>
2022-08-12 20:03:44 +0700

    Add font height styling in layout component

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -2,7 +2,7 @@ import MainNavigation from "./MainNavigation";
 
 export default function Layout(props) {
     return (
-        <div className="tracking-widest bg-[#323232] h-screen">
+        <div className="tracking-widest leading-relaxed bg-[#323232] text-white h-screen">
             <MainNavigation />
             <main>{props.children}</main>
         </div>
