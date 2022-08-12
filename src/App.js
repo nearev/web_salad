a42c989a588c602d1767d038704c27909a5a24ea
Jason Limas <jasonandrea14@gmail.com>
2022-08-12 19:35:28 +0700

    Refactored components name & added about route

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/layout/Layout";
-import Home from "./pages/HomePage";
-import Projects from "./pages/ProjectsPage";
+import HomePage from "./pages/HomePage";
+import ProjectsPage from "./pages/ProjectsPage";
+import AboutPage from "./pages/AboutPage";
+
 function App() {
     return (
         <Layout>
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/projects" element={<Projects />} />
+                <Route path="/" element={<HomePage />} />
+                <Route path="/projects" element={<ProjectsPage />} />
+                <Route path="/about" element={<AboutPage />} />
             </Routes>
         </Layout>
     );
