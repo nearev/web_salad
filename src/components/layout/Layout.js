8d5b6e80b18d859affc6ac3f182b2cf1841b5bf5
Jason Limas <jasonandrea14@gmail.com>
2022-08-12 19:35:55 +0700

    Add background and height to layout

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -2,7 +2,7 @@ import MainNavigation from "./MainNavigation";
 
 export default function Layout(props) {
     return (
-        <div className="tracking-widest">
+        <div className="tracking-widest bg-[#323232] h-screen">
             <MainNavigation />
             <main>{props.children}</main>
         </div>
