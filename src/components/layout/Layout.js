6d4daa9cd75b0f23ec34e0ec2a44c75f3c149a6b
Jason Limas <jasonandrea14@gmail.com>
2022-08-13 02:23:53 +0700

    Add Footer component to Layout component

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,10 +1,12 @@
 import MainNavigation from "./MainNavigation";
+import Footer from "../bottombar/Footer";
 
 export default function Layout(props) {
     return (
         <div className="tracking-widest leading-relaxed bg-[#323232] text-white h-screen">
             <MainNavigation />
             <main>{props.children}</main>
+            <Footer />
         </div>
     );
 }
