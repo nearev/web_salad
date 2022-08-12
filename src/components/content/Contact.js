d298ec337c1fbe49d18dd9b6cc5c6bb2fc9c11b6
Jason Limas <jasonandrea14@gmail.com>
2022-08-12 22:42:52 +0700

    Add form without button in Contact page

--- src/components/content/Contact.js
+++ src/components/content/Contact.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function About() {
     return (
-        <div className="pt-52 text-center">
+        <div className="pt-52 text-center items-center flex justify-center flex-col">
             <h2 className="font-bold text-3xl">CONTACT US</h2>
             <p className="font-semibold mt-10">Gedung Ruko 3</p>
             <p className="font-thin">
@@ -15,17 +15,17 @@ export default function About() {
 
             {/* Contact form */}
             <form className="mt-10">
-                <div className="flex flex-col items-center">
-                    <label className="font-semibold text-lg">Name</label>
-                    <input type="text" className="w-full px-4 py-2 border border-gray-400 rounded-lg" />
-                </div>
-                <div className="flex flex-col items-center mt-4">
-                    <label className="font-semibold text-lg">Email</label>
-                    <input type="text" className="w-full px-4 py-2 border border-gray-400 rounded-lg" />
-                </div>
-                <div className="flex flex-col items-center mt-4">
-                    <label className="font-semibold text-lg">Message</label>
-                    <textarea className="w-full px-4 py-2 border border-gray-400 rounded-lg" />
+                <div className="flex flex-row">
+                    <div className="flex flex-col text-right p-3">
+                        <label htmlFor="name" className="mb-3 p-1">Name</label>
+                        <label htmlFor="email" className="mb-3 p-1">Your Email</label>
+                        <label htmlFor="message" className="mb-3 p-1">Message/Question</label>
+                    </div>
+                    <div className="flex flex-col p-3">
+                        <input  className="mb-3 p-1 w-96 rounded-sm" type="text" id="name" placeholder="Your Name" />
+                        <input  className="mb-3 p-1 w-96 rounded-sm" type="text" id="email" placeholder="Your Email" />
+                        <textarea className="mb-3 p-1 w-96 rounded-sm" id="message" placeholder="Your Message" />
+                    </div>
                 </div>
             </form>
 
