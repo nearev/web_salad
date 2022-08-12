7317edfe7afa12444ff5f2c913969794a675c59e
Jason Limas <jasonandrea14@gmail.com>
2022-08-12 19:36:10 +0700

    Add AboutPage component

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import About from "../components/content/About";
+
+export default function AboutPage() {
+    return (
+        <main>
+            <About />
+        </main>
+    );
+}
