d389ca551be4d7093e153cf70fc38e3505b44d9e
Axell MS <axellms18@gmail.com>
2023-04-01 21:30:59 +0700

    Carousel using swiperjs

--- src/components/banner/HomeBanner.js
+++ src/components/banner/HomeBanner.js
@@ -1,15 +1,10 @@
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import { useState, useEffect } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Autoplay, Navigation, Pagination } from "swiper";
+import "swiper/swiper-bundle.css";
 export default function HomeBanner() {
-    // const backgrounds = [
-    //     {
-    //         backgroundImage: "url(assets/citra.jpg)",
-    //     },
-    //     {
-    //         backgroundImage: "url(assets/sasa.jpg)",
-    //     },
-    // ];
     // const [currIdx, setCurrIdx] = useState(1);
     // const [style, setStyle] = useState({
     //     backgroundImage: "url(assets/citra.jpg)",
@@ -37,6 +32,7 @@ export default function HomeBanner() {
         });
     };
 
+    SwiperCore.use([Navigation, Pagination, Autoplay]);
     useEffect(() => {
         // const img = new Image();
         // img.src = src;
@@ -89,92 +85,133 @@ export default function HomeBanner() {
             //     <div className="absolute h-full w-full bg-black opacity-50 top-0 left-0" />
             // </div>
 
-            <div
-                id="mycarousel"
-                className="flex carousel h-full w-full slide relative 2xl:w-[100%] 2xl:h-screen xl:w-auto xl:h-screen lg:h-screen lg:w-auto md:h-screen md:w-auto sm:h-screen sm:w-auto"
-                data-bs-ride="carousel"
-            >
-                <ol className="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4 z-[100] ">
-                    <li
-                        type="button"
-                        data-bs-target="#mycarousel"
-                        data-bs-slide-to="0"
-                        className="active"
-                        aria-current="true"
-                        style={styles}
-                    ></li>
-                    <li
-                        type="button"
-                        data-bs-target="#mycarousel"
-                        data-bs-slide-to="1"
-                        style={styles}
-                    ></li>
-                    <li
-                        type="button"
-                        data-bs-target="#mycarousel"
-                        data-bs-slide-to="2"
-                        style={styles}
-                    ></li>
-                </ol>
-                {/* item */}
-                <div className="carousel-inner relative w-full h-full overflow-hidden font-['Montserrat'] text-[90px] tracking-wider">
-                    <div className=" carousel-item active relative float-left w-full text-white h-full ">
-                        <img
-                            // src="assets/Kamen-Rider-drive.png"
-                            src={images[0].src}
-                            alt="houseCitra"
-                            className="absolute w-full object-cover h-full"
-                        />
-                        {/* <div
-                            style={{
-                                backgroundImage: `url(assets/${images[0].src})`,
-                            }}
-                        /> */}
+            // <div
+            //     id="mycarousel"
+            //     className="flex carousel h-full w-full slide relative 2xl:w-[100%] 2xl:h-screen xl:w-auto xl:h-screen lg:h-screen lg:w-auto md:h-screen md:w-auto sm:h-screen sm:w-auto"
+            //     data-bs-ride="carousel"
+            // >
+            //     <ol className="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4 z-[100] ">
+            //         <li
+            //             type="button"
+            //             data-bs-target="#mycarousel"
+            //             data-bs-slide-to="0"
+            //             className="active"
+            //             aria-current="true"
+            //             style={styles}
+            //         ></li>
+            //         <li
+            //             type="button"
+            //             data-bs-target="#mycarousel"
+            //             data-bs-slide-to="1"
+            //             style={styles}
+            //         ></li>
+            //         <li
+            //             type="button"
+            //             data-bs-target="#mycarousel"
+            //             data-bs-slide-to="2"
+            //             style={styles}
+            //         ></li>
+            //     </ol>
+            //     {/* item */}
+            //     <div className="carousel-inner relative w-full h-full overflow-hidden font-['Montserrat'] text-[90px] tracking-wider">
+            //         <div className=" carousel-item active relative float-left w-full text-white h-full ">
+            //             <img
+            //                 // src="assets/Kamen-Rider-drive.png"
+            //                 src={images[0].src}
+            //                 alt="houseCitra"
+            //                 className="absolute w-full object-cover h-full"
+            //             />
+            //             {/* <div
+            //                 style={{
+            //                     backgroundImage: `url(assets/${images[0].src})`,
+            //                 }}
+            //             /> */}
 
+            //             <span className="absolute bottom-0 left-5 z-50 opacity-80 pb-4">
+            //                 Citra Home
+            //             </span>
+            //         </div>
+            //         <div className=" carousel-item relative float-left w-full text-white h-full">
+            //             <img
+            //                 // src="assets/Date_Birth_Profile.jpg"
+            //                 src={images[1].src}
+            //                 alt="sasa"
+            //                 className="absolute h-full w-full object-cover"
+            //             />
+            //             <span className="absolute bottom-0 left-5 z-50 opacity-80 pb-4">
+            //                 Sasa Restaurant
+            //             </span>
+            //         </div>
+            //         <div className=" carousel-item relative float-left w-full text-white h-full">
+            //             <img
+            //                 // src="assets/kantor.webp"
+            //                 src={images[2].src}
+            //                 alt="kantor"
+            //                 className="absolute h-full w-full object-cover"
+            //             />
+            //             <span className="absolute bottom-0 left-5 z-50 opacity-80 pb-4">
+            //                 Office ABC
+            //             </span>
+            //         </div>
+            //     </div>
+            //     {/* background dimmer */}
+            //     <div className="absolute h-full w-full bg-black opacity-50 top-0 left-0 flex items-center justify-center pt-[90px]"></div>
+
+            //     <button
+            //         className="carousel-control-prev absolute top-[90px] bottom-0  flex items-center justify-around hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0 w-20"
+            //         data-bs-target="#mycarousel"
+            //         data-bs-slide="prev"
+            //     >
+            //         <ArrowBackIosIcon />
+            //     </button>
+            //     <button
+            //         className="carousel-control-next absolute top-[90px] bottom-0 flex items-center justify-around hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0 w-20"
+            //         data-bs-target="#mycarousel"
+            //         data-bs-slide="next"
+            //     >
+            //         <ArrowForwardIosIcon />
+            //     </button>
+            // </div>
+
+            <div className="h-screen text-[90px]">
+                <Swiper
+                    slidesPerView={1}
+                    autoplay={{
+                        delay: 4000,
+                        disableOnInteraction: false,
+                    }}
+                    className="w-full h-full"
+                    loop
+                >
+                    <SwiperSlide>
+                        <img
+                            src={images[0].src}
+                            className="absolute w-full object-cover h-full opacity-50"
+                        />
                         <span className="absolute bottom-0 left-5 z-50 opacity-80 pb-4">
                             Citra Home
                         </span>
-                    </div>
-                    <div className=" carousel-item relative float-left w-full text-white h-full">
+                    </SwiperSlide>
+                    <SwiperSlide>
                         <img
-                            // src="assets/Date_Birth_Profile.jpg"
                             src={images[1].src}
-                            alt="sasa"
-                            className="absolute h-full w-full object-cover"
+                            className="absolute w-full object-cover h-full opacity-50"
                         />
                         <span className="absolute bottom-0 left-5 z-50 opacity-80 pb-4">
-                            Sasa Restaurant
+                            Office Abc
                         </span>
-                    </div>
-                    <div className=" carousel-item relative float-left w-full text-white h-full">
+                    </SwiperSlide>
+                    <SwiperSlide>
                         <img
-                            // src="assets/kantor.webp"
                             src={images[2].src}
-                            alt="kantor"
-                            className="absolute h-full w-full object-cover"
+                            className="absolute w-full object-cover h-full opacity-50"
                         />
                         <span className="absolute bottom-0 left-5 z-50 opacity-80 pb-4">
-                            Office ABC
+                            Gedung Beta
                         </span>
-                    </div>
-                </div>
-                {/* background dimmer */}
+                    </SwiperSlide>
+                </Swiper>
                 <div className="absolute h-full w-full bg-black opacity-50 top-0 left-0 flex items-center justify-center pt-[90px]"></div>
-
-                <button
-                    className="carousel-control-prev absolute top-[90px] bottom-0  flex items-center justify-around hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0 w-20"
-                    data-bs-target="#mycarousel"
-                    data-bs-slide="prev"
-                >
-                    <ArrowBackIosIcon />
-                </button>
-                <button
-                    className="carousel-control-next absolute top-[90px] bottom-0 flex items-center justify-around hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0 w-20"
-                    data-bs-target="#mycarousel"
-                    data-bs-slide="next"
-                >
-                    <ArrowForwardIosIcon />
-                </button>
             </div>
         )
     );
--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import Gallery from "../components/content/Gallery";
 import { Swiper, SwiperSlide } from "swiper/react";
