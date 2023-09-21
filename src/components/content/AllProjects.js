503987e2876d9ae0a4d84a26a1370104250c1386
Axell MS <92212210+nearev@users.noreply.github.com>
2023-09-21 20:49:00 +0700

    Update AllProjects.js

--- src/components/content/AllProjects.js
+++ src/components/content/AllProjects.js
@@ -13,39 +13,18 @@ export default function AllProjects({ dummyData }) {
     }
 
     return (
-        <div>
-            <div className="grid grid-cols-4 h-screen">
+        // buat centerin
+        <div className="flex justify-center h-screen">
+            <div className="md:flex md:flex-row max-w-[1000px] flex-col items-center w-full h-full md:pt-0 pt-24">
                 {dummyData.type.map((type) => (
-                    <div className="flex items-center justify-center">
-                        <button className="peer transition ease-in-out duration-200 hover:scale-110">
+                    <div className="md:w-[20rem] h-[10rem] flex justify-center">
+                        <button className="h-auto peer transition ease-in-out duration-200 hover:scale-110">
                             <Link to={`/projects/${type.name}`}>
-                                <span className="tracking-widest">
+                                <span className="tracking-widest block md:inline-block">
                                     {type.name}
                                 </span>
                             </Link>
                         </button>
-
-                        {/* <ul className="hidden peer-hover:flex hover:flex flex flex-col peer-hover:h-auto hover:h-auto">
-                                    {type.projects.map((project) => (
-                                        <li className="mt-4 cursor-pointer transition ease-in-out hover:scale-110">
-                                            <Link
-                                                to={`/projects/${project.id}`}
-                                            >
-                                                {project.name}
-                                            </Link>
-                                        </li>
-                                    ))} */}
-
-                        {/* <Modal
-                                isOpen={open}
-                                onRequestClose={closeModal}
-                                className={classes.Modal}
-                                overlayClassName={classes.Overlay}
-                            >
-                                <span>Testing</span> <br></br>
-                                <button onClick={closeModal}>Close</button>
-                            </Modal> */}
-                        {/* </ul> */}
                     </div>
                 ))}
             </div>
