cd325c5b538a02e45b37bc22d1cb6e9bef886101
Axell MS <92212210+nearev@users.noreply.github.com>
2023-12-13 09:26:58 +0700

    progress on mobile menu burer

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -4,7 +4,7 @@ import Footer from "../bottombar/Footer";
 export default function Layout(props) {
     return (
         <div className="font-['Lato'] tracking-widest leading-relaxed">
-            <div className="bg-[#323232] text-white min-h-screen h-full">
+            <div className={`bg-[#323232] text-white min-h-screen h-full`}>
                 <div>
                     <MainNavigation />
                     <div className="h-auto ">{props.children}</div>
--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
+import { Squash as Hamburger } from "hamburger-react";
 import { Link } from "react-router-dom";
+import { motion, AnimatePresence } from "framer-motion";
 export default function MainNavigation() {
     const [open, setOpen] = useState(false);
 
@@ -20,26 +22,13 @@ export default function MainNavigation() {
                         className="w-[75px] h-[75px]"
                     />
                 </Link>
+                
             </div>
             {/* Links / Navbar */}
-            <nav className="flex relative justify-end px-5 items-center h-full font-['Lato'] font-light italic text-[18px] z-20">
-                <div className="md:hidden">
-                    <button onClick={toggleMenu}>
-                        <svg
-                        className="w-6 h-6 text-white"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                        >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M4 6h16M4 12h16M4 18h16"
-                        ></path>
-                        </svg>
-                    </button>
+            <AnimatePresence>
+            <motion.nav  className={`flex relative justify-end px-5 items-center h-full font-['Lato'] font-light italic text-[18px] z-10 `}>
+                <div className="md:hidden ">
+                    <Hamburger toggled={open} toggle={toggleMenu} />                   
                 </div>
                 
                 <div className="md:block hidden">
@@ -56,39 +45,35 @@ export default function MainNavigation() {
                         <Link to="/about">About</Link>
                     </span>
                 </div>
-
-                {open && (
-                    <div
-                    className={`fixed top-0 right-0 w-full h-full bg-zinc-900 opacity-95 flex flex-col justify-center items-center z-50 transition-transform duration-300 ${
-                      open ? 'translate-x-0' : 'translate-x-full'
-                    }`}
-                  >
-                        <button
-                        onClick={closeMenu}
-                        className="absolute top-5 right-3 text-white text-2xl"
-                        >
-                            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-                                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-                            </svg>
-                        </button>
-                        <ul className="text-2xl font-bold text-white">
-                            <li className="mb-8 hover:font-bold transition-all duration-200 cursor-pointer" onClick={closeMenu}>
-                                <Link to="/">Home</Link>
-                            </li>
-                            <li className="mb-8 hover:font-bold transition-all duration-200 cursor-pointer" onClick={closeMenu}>
-                                <Link to="/projects">Projects</Link>
-                            </li>
-                            <li className="mb-8 hover:font-bold transition-all duration-200 cursor-pointer" onClick={closeMenu}>
-                                <Link to="/contact">Contact</Link>
-                            </li>
-                            <li className="mb-8 hover:font-bold transition-all duration-200 cursor-pointer" onClick={closeMenu}>
-                                <Link to="/about">About</Link>
-                            </li>
-                        </ul>
-                  </div>
-                )}
                 
-            </nav>
+                    {open && (
+                        <motion.div
+                        initial={{ opacity: 0}}
+                        animate={{opacity:1}}
+                        exit={{opacity:0}}
+                        transition={{duration: 0.3}}
+                        
+                        className={`fixed top-[5.5rem] right-0 w-full shadow-xl flex justify-center  rounded-b-lg`}
+                    >
+                        <ul className="text-2xl font-bold text-white">
+                            <motion.li className="mb-8 hover:font-bold transition-all duration-200 cursor-pointer" onClick={closeMenu}>
+                                <Link to="/">Home</Link>
+                            </motion.li>
+                            <motion.li className="mb-8 hover:font-bold transition-all duration-200 cursor-pointer" onClick={closeMenu}>
+                                <Link to="/projects">Projects</Link>
+                            </motion.li>
+                            <motion.li className="mb-8 hover:font-bold transition-all duration-200 cursor-pointer" onClick={closeMenu}>
+                                <Link to="/contact">Contact</Link>
+                            </motion.li>
+                            <motion.li className="mb-8 hover:font-bold transition-all duration-200 cursor-pointer" onClick={closeMenu}>
+                                <Link to="/about">About</Link>
+                            </motion.li>
+                        </ul>
+                    </motion.div>
+                    )}
+                
+            </motion.nav>
+            </AnimatePresence>
             {/* darken background */}
             <div className="absolute h-full w-full bg-[#1A1A1A] opacity-0 top-0 left-0" />
         </div>
